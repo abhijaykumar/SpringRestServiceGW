2aec398325004e1459b990c32045f5c5ed522b26
Abhijay Kumar <abhijay.kumar@centricconsulting.com>
2019-09-10 17:38:46 +0530

    Added Spring boot servlet initializer for tomcat

--- src/main/java/com/centric/SpringGwApplication.java
+++ src/main/java/com/centric/SpringGwApplication.java
@@ -2,11 +2,26 @@ package com.centric;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class SpringGwApplication {
+@RestController
+public class SpringGwApplication extends SpringBootServletInitializer {
 
 	public static void main(String... args) {
 		SpringApplication.run(SpringGwApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return  builder.sources(SpringGwApplication.class);
+	}
+
+	@RequestMapping("/")
+	public String welcome() {
+		return "Welcome to Spring Boot REST Web service!";
 	}
 }
